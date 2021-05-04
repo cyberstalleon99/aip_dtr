aebc67c6ca0b3d7437a02f5142c11e7efaf0ea2a
dongilayAIP <clydekhayad.aip@gmail.com>
2021-05-04 09:11:15 +0800

    Working DTR of Sison

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ PARENT_DIR = '/home/pi/aip'
 DB_PATH = os.path.join(PARENT_DIR, 'dtr/dtr_app/aip_dtr.db')
 PICKLE_FILE_PATH = os.path.join(PARENT_DIR, 'dtr/dtr_app/encodings.pickle')
 XML_FILE_PATH = os.path.join(PARENT_DIR, 'dtr/dtr_app/haarcascade_frontalface_default.xml')
-LOCATION = 'AIP Main'
+LOCATION = 'Sison'
 # Time before user can time in/out in minutes
 LOG_TIMEOUT = 5
 USE_LOGIN_BTN_TO_CAPTURE = True
