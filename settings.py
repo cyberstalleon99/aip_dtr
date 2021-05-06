cc77ee857c8c57b8a3dd7ef70315f4de57dc96cc
Clyde@AIP <clyde.khayad@gmail.com>
2021-05-06 12:49:03 +0800

    Added new employees from AIP Main

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ DB_PATH = os.path.join(PARENT_DIR, 'dtr/dtr_app/aip_dtr.db')
 DTR_EXTRACTS_PATH = os.path.join(PARENT_DIR, 'DTR_Extracts')
 PICKLE_FILE_PATH = os.path.join(PARENT_DIR, 'dtr/dtr_app/encodings.pickle')
 XML_FILE_PATH = os.path.join(PARENT_DIR, 'dtr/dtr_app/haarcascade_frontalface_default.xml')
-LOCATION = 'Sison'
+LOCATION = 'AIP Main'
 # Time before user can time in/out in minutes
 LOG_TIMEOUT = 5
 USE_LOGIN_BTN_TO_CAPTURE = True
